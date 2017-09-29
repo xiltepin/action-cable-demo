aa7703e609850d4f4a369b720ea2b3f10bd1cc63
Eduardo Sanchez <sanchez.eduardo@synergy101.jp>
2017-09-29 14:31:49 +0900

    refs done

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,14 @@
+#app/channels/room_channel.rb
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    Message.create! content: data['message']
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+#config/routes.rb
+Rails.application.routes.draw do
+  root to: 'rooms#show'
+
+  mount ActionCable.server => '/cable'
+end
